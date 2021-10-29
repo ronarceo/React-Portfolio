4258a747f6058014d8ada07b252165a8bc1374e6
ronarceo <ronaldmarceo@gmail.com>
2021-10-29 16:24:56 -0500

    made files for necessary components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <Header />
+      <Navigation />
+      <Project />
+      <Footer />
+    </main>
+  );
 }
