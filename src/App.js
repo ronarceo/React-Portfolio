458d2c9ab26fb167a58f31aab82e1b52b0dbde71
ronarceo <ronaldmarceo@gmail.com>
2021-10-31 17:53:01 -0500

    added github and linkedin icons to footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <main>
       <Project />
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,20 @@
-function Footer() {};
+import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+    return (
+        <footer>
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a href="https://github.com/ronarceo" target="_blank">
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/ronald-arceo" target="_blank" >
+                    <FaLinkedinIn />
+                </a>
+            </div>
+        </footer>
+    );
+}
+
 
 export default Footer;
