2de099eac88e9c3e49f17b4885fd0c3e2a83f968
ronarceo <ronaldmarceo@gmail.com>
2021-10-31 21:22:46 -0500

    added text and picture in about component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
+            About Me
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          Full stack web developer that specializes in HTML, CSS, JavaScript, and React on the front end and Node, Express, SQL, and MongoDB on the back end.
+          </p>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="profile"
+            src="./profile pic.jpg"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
