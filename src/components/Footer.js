3cc354895b8c7e91211b6f690aa8550093502d1e
ronarceo <ronaldmarceo@gmail.com>
2021-11-09 14:35:48 -0600

    increased size of footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ function Footer() {
     return (
         <footer>
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a href="https://github.com/ronarceo" target="_blank">
-                    <FaGithub />
+                <a href="https://github.com/ronarceo" target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={36} />
                 </a>
-                <a href="https://www.linkedin.com/in/ronald-arceo" target="_blank" >
-                    <FaLinkedinIn />
+                <a href="https://www.linkedin.com/in/ronald-arceo" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedinIn size={36} />
                 </a>
             </div>
         </footer>
