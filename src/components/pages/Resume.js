4ee68f7f4c2064c0df2e00ace26022798e047550
ronarceo <ronaldmarceo@gmail.com>
2021-11-09 11:16:49 -0600

    added resume and download link to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 export default function Resume() {
-    return (
-      <div>
-        <h1>Resume</h1>
-      </div>
-    );
-  }
+  return (
+    <section>
+      <h3>Front-End Proficiencies:</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>React</li>
+        <li>Bootsrap</li>
+      </ul>
+      <h3>Back-End Proficiencies:</h3>
+      <ul>
+        <li>API's</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>MongoDB, Mongoose</li>
+        <li>MERN Stack</li>
+      </ul>
+      <iframe
+        src="https://drive.google.com/file/d/1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz/preview"
+        allow="autoplay"
+        title="Ronald Arceo Resume"
+        height="500" width="100%"
+      ></iframe>
+      <a
+        className="download "
+        href="https://drive.google.com/uc?export=download&id=1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz"
+      >
+        Download Here
+      </a>
+    </section>
+  )
+}
