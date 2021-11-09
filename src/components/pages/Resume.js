723facc34cdd172459cbb5806b8b971709f9b155
ronarceo <ronaldmarceo@gmail.com>
2021-11-09 16:02:30 -0600

    removed proficiencies from resume page since resume is displayed

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,29 +3,13 @@ import React from 'react';
 export default function Resume() {
   return (
     <section>
-      <div className="container px-5 py-10 mx-auto">
-      <h3>Front-End Proficiencies:</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
-      </ul>
-      <h3>Back-End Proficiencies:</h3>
-      <ul>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySQL</li>
-        <li>MongoDB</li>
-      </ul>
-      </div>
       <div className="text-center mb-20">
-      <a
-        className="download "
-        href="https://drive.google.com/uc?export=download&id=1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz"
-      >
-        Click here to download my resume
-      </a>
+        <a
+          className="download "
+          href="https://drive.google.com/uc?export=download&id=1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz"
+        >
+          Click here to download my resume
+        </a>
       </div>
       <iframe
         src="https://drive.google.com/file/d/1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz/preview"
