a57320b99b1f5bcfcef37918f414f8c16490a0e7
ronarceo <ronaldmarceo@gmail.com>
2021-11-09 15:51:34 -0600

    updated styling on resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Resume() {
   return (
     <section>
+      <div className="container px-5 py-10 mx-auto">
       <h3>Front-End Proficiencies:</h3>
       <ul>
         <li>HTML</li>
@@ -17,18 +18,21 @@ export default function Resume() {
         <li>MySQL</li>
         <li>MongoDB</li>
       </ul>
+      </div>
+      <div className="text-center mb-20">
+      <a
+        className="download "
+        href="https://drive.google.com/uc?export=download&id=1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz"
+      >
+        Click here to download my resume
+      </a>
+      </div>
       <iframe
         src="https://drive.google.com/file/d/1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz/preview"
         allow="autoplay"
         title="Ronald Arceo Resume"
         height="500" width="100%"
       ></iframe>
-      <a
-        className="download "
-        href="https://drive.google.com/uc?export=download&id=1fR6FpIPe0sDAepiJ39HUEuBcijbXWzdz"
-      >
-        Download Here
-      </a>
     </section>
   )
 }
